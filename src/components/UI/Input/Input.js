e237c3de2a63dfa9437b1b4248eec12596ed28d1
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-03 13:05:31 +0400

    Create a JS config for the form and generate the form dynamically from that

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,39 @@ import Classes from './Input.css';
 const input = (props) => {
     let inputElement = null;
 
-    switch(props.inputtype) {
+    switch(props.elementType) {
         case 'input':
-            inputElement = <input className={Classes.InputElement} {...props} />;
+            inputElement = <input 
+                className={Classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value} />;
         break;
         case 'textarea':
-            inputElement = <textarea className={Classes.InputElement} {...props} />;
+            inputElement = <textarea 
+                className={Classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value} />;
+        break;
+        case 'select':
+            inputElement = (
+                <select 
+                    className={Classes.InputElement}  
+                    value={props.value} >
+                    {props.elementConfig.options.map(option => (
+                        <option 
+                            key={option.value}
+                            value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
         break;
         default:
-            inputElement = <input className={Classes.InputElement} {...props} />;
+            inputElement = <input 
+                className={Classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value} />;
         break;
     }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,49 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name:  '',
-        email: '',
-        address: {
-            street:     '',
-            postalCode: ''
+        orderForm :{
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email address'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
+                    ]
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -23,16 +61,7 @@ class ContactData extends Component {
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price, //in a real app this should be calculated on the server to make sure that the user is not manipulating the price before sending it
-            customer: {
-                name: 'Mahmoud',
-                address: {
-                    country: 'UAE',
-                    city: 'Dubai'
-                },
-                email: 'mahmoud.srouji@gmail.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price, //in a real app this should be calculated on the server to make sure that the user is not manipulating the price before sending it   
         }
         axios.post('/orders.json', order).then(response => {
             this.setState({ 
@@ -49,12 +78,22 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
         let form =(
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your name" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your email" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal" />
+                { formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig} 
+                        value={formElement.config.value} />
+                )) }
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
         );
