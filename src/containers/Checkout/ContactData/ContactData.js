d387d57d3cf508d315d384ae215b8def55d3e5af
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-24 23:35:50 +0400

    Installing the Redux Devtools

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -33,7 +33,7 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    
+                    isEmail: true
                 },
                 valid: false,
                 touched: false
@@ -47,7 +47,6 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    
                 },
                 valid: false,
                 touched: false
@@ -123,6 +122,14 @@ class ContactData extends Component {
         }
         if(rules.maxLength) {
             isValid = value.length <= rules.maxLength && isValid;
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
         }
         return isValid;
     }
