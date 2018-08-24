5db5c36c7633a1971ebae97d84fe1ffadaa7acd9
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-25 00:28:38 +0400

    Add thunk middleware

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+    applyMiddleware(thunk) // the thunk middleware is unlocked and we can start using it
+));
 
 const app = (
     <Provider store={store}>
