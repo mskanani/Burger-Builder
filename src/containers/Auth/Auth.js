8e4a4b9ccbbd267acbf68395591cf9098d9036f8
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-28 00:03:40 +0400

    Add Auth actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     state = {
@@ -10,12 +13,12 @@ class Auth extends Component {
                 elementType: 'input',
                 elementConfig: {
                     type: 'email',
-                    placeholder: 'E-mail Address'
+                    placeholder: 'Mail Address'
                 },
                 value: '',
                 validation: {
                     required: true,
-                    isEmail: true,
+                    isEmail: true
                 },
                 valid: false,
                 touched: false
@@ -35,68 +38,82 @@ class Auth extends Component {
                 touched: false
             }
         }
-    };
+    }
 
     checkValidity(value, rules) {
         let isValid = true;
-
-        if(rules.required) {
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
-        if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
         }
+
         if (rules.isEmail) {
             const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
             isValid = pattern.test(value) && isValid
         }
+
         if (rules.isNumeric) {
             const pattern = /^\d+$/;
             isValid = pattern.test(value) && isValid
         }
+
         return isValid;
     }
 
     inputChangedHandler = (event, controlName) => {
         const updatedControls = {
-            ...this.state.controls, //the object will not copied deeply (no nested objects)
+            ...this.state.controls,
             [controlName]: {
                 ...this.state.controls[controlName],
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
             }
-        }
-        this.setState({controls:updatedControls});
+        };
+        this.setState({controls: updatedControls});
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
     }
 
     render () {
         const formElementsArray = [];
-        for(let key in this.state.controls) {
-            formElementsArray.push({
+        for ( let key in this.state.controls ) {
+            formElementsArray.push( {
                 id: key,
                 config: this.state.controls[key]
-            });
+            } );
         }
-        const form = formElementsArray.map(formElement => (
-            <Input 
+
+        const form = formElementsArray.map( formElement => (
+            <Input
                 key={formElement.id}
-                elementType={formElement.config.elementType} 
-                elementConfig={formElement.config.elementConfig} 
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
                 value={formElement.config.value}
                 invalid={!formElement.config.valid}
                 shouldValidate={formElement.config.validation}
                 touched={formElement.config.touched}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)} 
-            />
-        ));
-        return(
+                changed={( event ) => this.inputChangedHandler( event, formElement.id )} />
+        ) );
+
+        return (
             <div className={classes.Auth}>
-                <form>
-                    {form}  
+                <form onSubmit={this.submitHandler}>
+                    {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
             </div>
@@ -104,4 +121,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
