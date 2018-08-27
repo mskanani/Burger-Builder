cdecbe4500d2704d900b474bc17defaf114a07ec
Mahmoud alsouji <mahmoud.srouji@gmail.com>
2018-08-27 23:33:47 +0400

    Adding an Auth form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   /*state = {
@@ -24,6 +25,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
           { /* By defaul / is treat me as a prefix standard so both will load if we don't use exact attribute or the switch component.. in case of using switch without exact, the order of the components matters */ }
